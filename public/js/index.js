89dc674a647b1a6bcfb10c9084e258e22574ef85
Michael <freefaller@gmail.com>
2017-12-14 14:18:25 -0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'alice',
-    text: 'Hi Alice.'
-  });
 });
 
 socket.on('disconnect', () => {
@@ -14,5 +9,5 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', (message) => {
-  console.log('New message', message);
+  console.log('newMessage', message);
 });
