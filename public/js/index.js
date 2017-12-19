0b06cbbcf892a87c4960ffa89c9ec305381f0d82
Michael <freefaller@gmail.com>
2017-12-19 14:09:00 -0800

    Scroll to bottom if user is at or near bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  let messages = $('#messages');
+  let newMessage = messages.children('li:last-child');
 
+  // Heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -20,6 +35,7 @@ socket.on('newMessage', (message) => {
   });
   
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -32,6 +48,7 @@ socket.on('newLocationMessage', (message) => {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {
